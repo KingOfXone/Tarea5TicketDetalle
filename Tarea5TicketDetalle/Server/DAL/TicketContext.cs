8811c949df894a337b7c63367af4ce5cd29c3228
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-10-24 16:01:14 -0400

    Agregando los componentes de tickers y la Db

---
+++ Tarea5TicketDetalle/Server/DAL/TicketContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketDetalle.Shared;
+
+namespace TicketDetalle.Server.DAL
+{
+    public class TicketContext : DbContext
+    {
+        public DbSet<Tickets> Tickets { get; set; }
+        public TicketContext(DbContextOptions<TicketContext> options) : base(options) { }
+    }
+}
