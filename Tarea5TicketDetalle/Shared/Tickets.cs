3bce95371fc945623d263e650d6a1d5433713391
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-10-24 16:24:00 -0400

    Agregando el microsoftframework y la carpeta shared

---
+++ Tarea5TicketDetalle/Shared/Tickets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tarea5TicketDetalle.Shared
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Es necesario completar el campo SolicitadoPor")]
+        public string? SolicitadoPor { get; set; } // Persona que solicita
+
+        [Required(ErrorMessage = "Es necesario completar el campo Asunto")]
+        public string? Asunto { get; set; } // Asunto del ticket
+
+        [Required(ErrorMessage = "Es necesario completar el campo Descripcion")]
+        public string? Descripcion { get; set; } // Descripción del ticket
+
+        [ForeignKey("TicketId")]
+        public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
+    }
+}
---
+++ Tarea5TicketDetalle/Shared/TicketsDetalle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea5TicketDetalle.Shared
+{
+    public class TicketsDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public string? Emisor { get; set; }
+        public string? Mensaje { get; set; }
+
+    }
+}
